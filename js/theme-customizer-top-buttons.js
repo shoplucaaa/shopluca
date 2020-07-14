f1e021d50ed1a3223e05244cb19fd2246866b7fd
shoplucaaa <68089849+shoplucaaa@users.noreply.github.com>
2020-07-14 18:09:31 +0700

    new bb

--- js/theme-customizer-top-buttons.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.wp-full-overlay-sidebar-content').prepend('<a style="width: 80%; margin: 10px auto; display: block; text-align: center;" href="https://quemalabs.ticksy.com/articles/100000146/" class="button" target="_blank">{documentation}</a>'.replace( '{documentation}', topbtns.documentation ) );
- 	$('.wp-full-overlay-sidebar-content').prepend('<a style="width: 80%; margin: 10px auto; display: block; text-align: center;" href="https://www.quemalabs.com/theme/shophistic-lite/?utm_source=Shophistic%20Lite%20Theme&utm_medium=Pro%20Button&utm_campaign=Shophistic" class="button" target="_blank">{pro}</a>'.replace( '{pro}', topbtns.pro ) );
-});
