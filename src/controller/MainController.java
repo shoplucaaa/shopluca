d2a3f210c0795bcdf1ba326a243610732d283040
shoplucaaa <68089849+shoplucaaa@users.noreply.github.com>
2020-09-17 20:50:37 +0700

    update luca maven

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,14 +1,20 @@
 package controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
@@ -25,36 +31,66 @@ public class MainController {
 		this.productDAO = productDAO;
 	}
 	
+	
 	@RequestMapping("/product")
 	public String product(Model m, HttpServletRequest request, HttpServletResponse response, CookieLocaleResolver clr) {	
 		
 		
-	
-		
 		request.getSession().setAttribute("system_name", "system name");
-
-		m.addAttribute("yourname", "Name");
-
-
 //		clr.setLocale(request, response, new Locale("vi"));
 
 		List<Product> list = productDAO.selectAll();
 		m.addAttribute("list", list);
 		
 	return "product";
+}
 	
+	@RequestMapping("/adminlogin")
+	public String adminlogin(Model m, HttpServletRequest request, HttpServletResponse response, CookieLocaleResolver clr) {	
+		
+		
+		request.getSession().setAttribute("system_name", "system name");
+//		clr.setLocale(request, response, new Locale("vi"));
+
+		
+	return "adminlogin";
+}
+	
+	
+	
+	
+	@RequestMapping("/login")
+	public String login(Model m, HttpServletRequest request, HttpServletResponse response, CookieLocaleResolver clr, Model l) {	
+				
+		request.getSession().setAttribute("system_name", "system name");
+
+		
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+
+		Pattern pattern = Pattern.compile("^nam");
+		Matcher matcher = pattern.matcher(password);
+		boolean matchFound = matcher.find();
+
+					
+			if (matchFound) {
+				return "editproduct"; 
+			} else {
+				m.addAttribute("msg", "Username or password incorrect");
+				return "adminlogin";
+			}	
 	
 }
+	
+	
 	
 	@RequestMapping("/index")
 	public String index(Model m, HttpServletRequest request, HttpServletResponse response, CookieLocaleResolver clr) {	
 		
-		
-	
-		
 		request.getSession().setAttribute("system_name", "system name");
 
-		m.addAttribute("yourname", "Name");
 
 
 //		clr.setLocale(request, response, new Locale("vi"));
